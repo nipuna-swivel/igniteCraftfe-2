c41c5a07e53db0c3843a916282f517d48aa43955
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-06-26 18:33:47 +0530

    hookforms

--- src/components/organisms/loginForm/index.tsx
+++ src/components/organisms/loginForm/index.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/atoms/button";
 import Input from "@/components/atoms/input";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { Alert, Space } from 'antd';
 
 type Inputs = {
 	username: string,
@@ -9,13 +10,16 @@ type Inputs = {
 
 function LoginForm() {
 
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit,watch,formState:{errors} } = useForm();
+	console.log(errors);
+	console.log(watch())
+	const fn= watch('username')
 	return (
 		<>
 			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
 					<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-						Sign in to your account
+						Sign in to your account-{fn}
 					</h2>
 				</div>
 
@@ -30,9 +34,11 @@ function LoginForm() {
 								User Name
 							</label>
 							<div className="mt-2">
+	
+								<p className="text-rose-700">{errors.username?.message}</p>
 								<input
 									id={"username"}									
-									{...register('username')}
+									{...register('username',{required:'Username required'})}
 									type={"username"}
 									autoComplete={"username"}
 									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -49,9 +55,10 @@ function LoginForm() {
 								</label>
 							</div>
 							<div className="mt-2">
+							<p className="text-rose-700">{errors.password?.message}</p>
 								<input
 									id={"password"}
-									{...register('password')}
+									{...register('password',{required:'Password required'})}
 									type={"password"}
 									autoComplete={"password"}									
 									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
