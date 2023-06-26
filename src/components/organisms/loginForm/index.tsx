bde6cb1dcf4d0bcf09512fbd69032611d82b6673
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-06-26 17:27:10 +0530

    axios and react hook form

--- src/components/organisms/loginForm/index.tsx
+++ src/components/organisms/loginForm/index.tsx
@@ -1,7 +1,15 @@
 import Button from "@/components/atoms/button";
 import Input from "@/components/atoms/input";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+	username: string,
+	password: string,
+  };
 
 function LoginForm() {
+
+	const { register, handleSubmit } = useForm();
 	return (
 		<>
 			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -12,17 +20,19 @@ function LoginForm() {
 				</div>
 
 				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-					<form className="space-y-6">
+					<form className="space-y-6" onSubmit={handleSubmit((data)=>{
+						console.log(data)
+					})}>
 						<div>
 							<label
 								htmlFor="email"
 								className="block text-sm font-medium leading-6 text-gray-900">
-								Email address
+								User Name
 							</label>
 							<div className="mt-2">
-								<Input
-									id={"username"}
-									name={"username"}
+								<input
+									id={"username"}									
+									{...register('username')}
 									type={"username"}
 									autoComplete={"username"}
 									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -39,9 +49,9 @@ function LoginForm() {
 								</label>
 							</div>
 							<div className="mt-2">
-								<Input
+								<input
 									id={"password"}
-									name={"password"}
+									{...register('password')}
 									type={"password"}
 									autoComplete={"password"}									
 									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -50,7 +60,7 @@ function LoginForm() {
 						</div>
 
 						<div>
-							<Button
+							<input
 								type={"submit"}
 								className={
 									"flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
