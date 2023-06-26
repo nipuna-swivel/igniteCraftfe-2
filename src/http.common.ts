c4987dc77fc32b6a16d6ed4ad0ed9b9b5fd20860
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-06-26 17:15:11 +0530

    setup axios

---
+++ src/http.common.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default axios.create({
+
+ baseURL: process.env.NEXT_PUBLIC_API_URL,
+ 
+  headers: {
+    "Content-type": "application/json",
+    
+  }
+  
+});
